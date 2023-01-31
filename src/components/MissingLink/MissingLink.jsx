9f53f246dc3c5610d864a09bdacda2f9546ae02b
Chris Maki <c.william.developer@gmail.com>
2023-01-30 19:28:37 -0600

    Fixed carousel height issue

--- src/components/MissingLink/MissingLink.jsx
+++ src/components/MissingLink/MissingLink.jsx
@@ -1,138 +1,81 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import Tabs from '@mui/material/Tabs';
-// import Tab from '@mui/material/Tab';
-// import Typography from '@mui/material/Typography';
-// import Box from '@mui/material/Box';
-
-// function TabPanel(props) {
-//   const { children, value, index, ...other } = props;
-
-//   return (
-//     <div
-
-//       role="tabpanel"
-//       hidden={value !== index}
-//       id={`vertical-tabpanel-${index}`}
-//       aria-labelledby={`vertical-tab-${index}`}
-//       {...other}
-//     >
-//       {value === index && (
-//         <Box sx={{ p: 3 }} >
-//           <Typography>{children}</Typography>
-//         </Box>
-//       )}
-//     </div>
-//   );
-// }
-
-// TabPanel.propTypes = {
-//   children: PropTypes.node,
-//   index: PropTypes.number.isRequired,
-//   value: PropTypes.number.isRequired,
-// };
-
-// function a11yProps(index) {
-//   return { 
-//     id: `vertical-tab-${index}`,
-//     'aria-controls': `vertical-tabpanel-${index}`,
-//   };
-// }
-
-//  function MissingLink() {
-//   const [value, setValue] = React.useState(0);
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
-
-//   return (
-//     <Box 
-//       sx={{ flexGrow: 1,  display: 'flex', height: 224 }}
-//     >
-//       <Tabs
-      
-//         orientation="vertical"
-//         variant="scrollable"
-//         value={value}
-//         onChange={handleChange}
-//         aria-label="Vertical tabs example"
-//         sx={{ borderRight: 1, borderColor: 'divider' }}
-//       >
-//         <Tab label="1" {...a11yProps(0)} />
-//         <Tab label="2" {...a11yProps(1)} />
-//         <Tab label="3" {...a11yProps(2)} />
-//         <Tab label="4" {...a11yProps(3)} />
-//         <Tab label="5" {...a11yProps(4)} />
-//         <Tab label="6" {...a11yProps(5)} />
-//         <Tab label="7" {...a11yProps(6)} />
-//         <Tab label="8" {...a11yProps(3)} />
-//         <Tab label="9" {...a11yProps(4)} />
-//         <Tab label="10" {...a11yProps(5)} />
-//         <Tab label="11" {...a11yProps(6)} />
-//       </Tabs>
-//       <TabPanel value={value} index={0} >
-//         <img src="/ignoredImages/mlink1.png" width="500px" height="250px"/>
-//       </TabPanel>
-//       <TabPanel value={value} index={1}>
-//         Item Two
-//       </TabPanel>
-//       <TabPanel value={value} index={2}>
-//         Item Three
-//       </TabPanel>
-//       <TabPanel value={value} index={3}>
-//         Item Four
-//       </TabPanel>
-//       <TabPanel value={value} index={4}>
-//         Item Five
-//       </TabPanel>
-//       </Box>
-//       )
-//       }
-
-// export default MissingLink;
-
-
-//CODE ABOVE IS KINDA SLOPPY LOOKING, HARD TO EDIT. CODE BELOW
-// IS USING A DIFFERENT METHOD, WE'LL SEE HOW IT LOOKS;
-import React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import "./MissingLink.css";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MobileStepper from "@mui/material/MobileStepper";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    label: "Login Page",
+    imgPath: "/ignoredImages/mlink1.png",
   },
   {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    label: "Landing Page / Search Page",
+    imgPath: "/ignoredImages/mlink2.png",
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+    label: "Search Page example",
+    imgPath: "/ignoredImages/mlink3.png",
   },
   {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    label: "Results Page",
+    imgPath: "/ignoredImages/mlink4.png",
   },
+  {
+    label: "Results Page condition comparisons",
+    imgPath: "/ignoredImages/mlink5.png",
+  },
+  {
+    label: "Results Page adding access key",
+    imgPath: "/ignoredImages/mlink6.png",
+  },
+  {
+    label: "Profile Page",
+    imgPath: "/ignoredImages/mlink7.png",
+  },
+  {
+    label: "Questions Page",
+    imgPath: "/ignoredImages/mlink8.png",
+  },
+  {
+    label: "Questions Page usage example",
+    imgPath: "/ignoredImages/mlink9.png",
+  }
 ];
 
 function SwipeableTextMobileStepper() {
+  const [isVisible, setIsVisible] = useState(false);
+  const location = useLocation();
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (!isFirstRender.current) {
+      setIsVisible(false);
+    }
+    isFirstRender.current = false;
+  }, [location.pathname]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 100);
+  }, [isVisible]);
+
+  const [checked, setChecked] = React.useState(false);
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
+
   const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
   const maxSteps = images.length;
@@ -150,75 +93,86 @@ function SwipeableTextMobileStepper() {
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
+    <div
+      className={`fade-in ${
+        isVisible ? "visible" : "hidden"
+      } landingPageContainer`}
+    >
+      <Box sx={{ maxWidth: '75%', flexGrow: 1 }}>
+        <Paper
+          square
+          elevation={0}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            height: 50,
+            pl: 2,
+            bgcolor: "background.default",
+          }}
+        >
+          <Typography>{images[activeStep].label}</Typography>
+        </Paper>
+        <AutoPlaySwipeableViews
+          axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+          interval={5000}
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          enableMouseEvents
+        >
+          {images.map((step, index) => (
+            <div key={step.label}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="img"
+                  sx={{
+                    height: '50%',
+                    display: "block",
+                    maxWidth: '100%',
+                    overflow: "hidden",
+                    width: "100%",
+                  }}
+                  src={step.imgPath}
+                  alt={step.label}
+                />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
+              Next
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowLeft />
+              ) : (
+                <KeyboardArrowRight />
+              )}
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              {theme.direction === "rtl" ? (
+                <KeyboardArrowRight />
+              ) : (
+                <KeyboardArrowLeft />
+              )}
+              Back
+            </Button>
+          }
+        />
+      </Box>
+    </div>
   );
 }
 
